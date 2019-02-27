e8332777eac6e3da20ad4210d5f456850f6f6138
anco5 <anco.dev5@gmail.com>
2019-02-28 00:41:16 +0900

    initial commit

---
+++ spring-boot-oauth-kotlin/src/main/kotlin/app/Controller.kt
@@ -0,0 +1,15 @@
+package app
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class Controller {
+
+    @GetMapping("")
+    fun home(): String {
+        return "home"
+    }
+
+}
+
